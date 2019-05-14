d358abd407194650aaab2bdac4293538278f5f72
heoegema <hollyoegema@gmail.com>
2019-05-14 11:54:17 -0400

    Changing port to 8000

--- source/entry.js
+++ source/entry.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const pg = require('./config/postgres')
 const app = express()
-const port = 3000
+const port = 8000
 
 app.get('/', function (req, res) {
   res.send('Home Page')
